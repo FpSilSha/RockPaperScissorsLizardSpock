6fe6bc7b64b690369f51d84479b2fdd4714b2a08
Fp38SilSha <faperez38s@gmail.com>
2019-01-29 00:29:16 -0600

    Program class: Added a way to replay the game or not. Added a catch for format issues involving replay question.
    Game class: Made the ability for payer to choose how many rounds out of 'x' games to beat. Moved some member variables around to handle scope issues.
    Wrote catches for possible input errors.
    Added a comparison function to compare gestures. Pretty much the logic of the entire game.
    Added a winner check function to see if either player has won, and to declare them as the winner.

--- RockPaperScissorsLizardSpock/RockPaperScissorsLizardSpock/Game.cs
+++ RockPaperScissorsLizardSpock/RockPaperScissorsLizardSpock/Game.cs
@@ -8,8 +8,13 @@ namespace RockPaperScissorsLizardSpock
 {
     public class Game
     {
+        Player playerOne;
+        Player playerTwo;
+        uint bestOfGames;
+    
        public void StartInstance()
-       {  
+       {    
+            
             int gameChoice = -1;
             do
           {
@@ -27,12 +32,31 @@ namespace RockPaperScissorsLizardSpock
           }
             while (gameChoice < 1 || gameChoice > 2);
 
+            Console.WriteLine("How many games to play to the best of? Default is 3 (best of 3). Enter an integer in Standard Form.");
+            try
+            {
+
+               uint bestOfResponse =  Convert.ToUInt32(Console.ReadLine());
+                bestOfGames = bestOfResponse / 2 + 1;
+
+            }
+            catch(FormatException)
+            {
+                Console.WriteLine("Send an email and explain to me how you do a 'best of' with the input you provided. Try Again.");
+
+            }
+
+
             if(gameChoice == 1)
             {
+                playerOne = new HumanPlayer();
+                playerTwo = new AIPlayer();
                 RunGame1();
             }
             else
             {
+                playerOne = new HumanPlayer();
+                playerTwo = new HumanPlayer();
                 RunGame2();
             }
 
@@ -41,23 +65,32 @@ namespace RockPaperScissorsLizardSpock
        
        public void RunGame1()
         {
-            Player playerOne = new HumanPlayer();
-            Player playerTwo = new AIPlayer();
             
-            playerOne.ChooseGesture();
-            playerTwo.ChooseGesture();
+            do
+            {
+                Console.WriteLine("\n\nNew Round!");
+                playerOne.ChooseGesture();
+                playerTwo.ChooseGesture();
+                CompareGesture();
+                WinnerCheck();
 
-
-
+            }
+            while (playerOne.score < bestOfGames && playerTwo.score < bestOfGames);
         }
 
         public void RunGame2()
         {
-            Player playerOne = new HumanPlayer();
-            Player playerTwo = new HumanPlayer();
-            
-            playerOne.ChooseGesture();
-            playerTwo.ChooseGesture();
+           do
+            {
+                Console.WriteLine("\n\nNew Round!");
+                playerOne.ChooseGesture();
+                playerTwo.ChooseGesture();
+                CompareGesture();
+                WinnerCheck();
+
+            }
+            while (playerOne.score < bestOfGames && playerTwo.score < bestOfGames);
+
         }
         public void DisplayRules()
         {
@@ -70,9 +103,119 @@ namespace RockPaperScissorsLizardSpock
 
         }
         
-       // public void 
+        public void CompareGesture()
+        {
+            switch(playerOne.gestureChoice)
+            {
+            case("Rock"):
+                    if(playerTwo.gestureChoice == playerOne.gestureChoice)
+                    {
+                        Console.WriteLine("Drat! It's a tie!");
+                        break;
+                    }
+                    else if(playerTwo.gestureChoice == "Scissors" || playerTwo.gestureChoice == "Lizard")
+                    {
+                        Console.WriteLine(playerOne.name + " wins! Point for "+playerOne.name + "!");
+                        playerOne.score++;
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine(playerTwo.name + " wins! Point for "+playerTwo.name + "!" );
+                        playerTwo.score++;
+                        break;
+                    }
+            case("Paper"):
+                    if(playerTwo.gestureChoice == playerOne.gestureChoice)
+                    {
+                        Console.WriteLine("Drat! It's a tie!");
+                        break;
+                    }
+                    else if(playerTwo.gestureChoice == "Rock" || playerTwo.gestureChoice == "Spock")
+                    {
+                        Console.WriteLine(playerOne.name + " wins! Point for "+playerOne.name + "!");
+                        playerOne.score++;
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine(playerTwo.name + " wins! Point for "+playerTwo.name + "!" );
+                        playerTwo.score++;
+                        break;
+                    }
+            case("Scissors"):
+                    if(playerTwo.gestureChoice == playerOne.gestureChoice)
+                    {
+                        Console.WriteLine("Drat! It's a tie!");
+                        break;
+                    }
+                    else if(playerTwo.gestureChoice == "Paper" || playerTwo.gestureChoice == "Lizard")
+                    {
+                        Console.WriteLine(playerOne.name + " wins! Point for "+playerOne.name + "!");
+                        playerOne.score++;
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine(playerTwo.name + " wins! Point for "+playerTwo.name + "!" );
+                        playerTwo.score++;
+                        break;
+                    }
+            case("Lizard"):
+                    if(playerTwo.gestureChoice == playerOne.gestureChoice)
+                    {
+                        Console.WriteLine("Drat! It's a tie!");
+                        break;
+                    }
+                    else if(playerTwo.gestureChoice == "Paper" || playerTwo.gestureChoice == "Spock")
+                    {
+                        Console.WriteLine(playerOne.name + " wins! Point for "+playerOne.name + "!");
+                        playerOne.score++;
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine(playerTwo.name + " wins! Point for "+playerTwo.name + "!" );
+                        playerTwo.score++;
+                        break;
+                    }
+            case("Spock"):
+                    if(playerTwo.gestureChoice == playerOne.gestureChoice)
+                    {
+                        Console.WriteLine("Drat! It's a tie!");
+                        break;
+                    }
+                    else if(playerTwo.gestureChoice == "Rock" || playerTwo.gestureChoice == "Scissors")
+                    {
+                        Console.WriteLine(playerOne.name + " wins! Point for "+playerOne.name + "!");
+                        playerOne.score++;
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine(playerTwo.name + " wins! Point for "+playerTwo.name + "!" );
+                        playerTwo.score++;
+                        break;
+                    }
 
-    
+            }
+        }
+
+        public void WinnerCheck()
+        {
+            if(playerOne.score == bestOfGames)
+            {
+                Console.WriteLine("\n\n\n" + playerOne.name + "Has won the game!!");
+            }
+            else if(playerTwo.score == bestOfGames);
+            {
+                Console.WriteLine("\n\n\n" + playerTwo.name + "Has won the game!!");
+
+            }
+
+        }
+
+
 
    
     }
--- RockPaperScissorsLizardSpock/RockPaperScissorsLizardSpock/Program.cs
+++ RockPaperScissorsLizardSpock/RockPaperScissorsLizardSpock/Program.cs
@@ -10,10 +10,34 @@ namespace RockPaperScissorsLizardSpock
     {
         static void Main(string[] args)
         {
-         Game nGame = new Game();
+            bool replay = true;
+            Game nGame = new Game();
             nGame.DisplayRules();
+            uint replayResponse;
+           do
+            {            
             nGame.StartInstance();
+            Console.WriteLine("\n\n\nWould you like to play again? Enter 1 for yes and 2 for no.");
+                do
+                {
+                    try
+                    {
+                        replayResponse = Convert.ToUInt32(Console.ReadLine());
+                    }
+                    catch(FormatException)
+                    {
+                        Console.WriteLine("You were given two options. How is it this hard? 1 OR 2 buddy. 1 OR 2.");
+                    }
+                }
+                while(!replayResponse == 1 || !replayResponse == 2);
+                if(replayResponse == 2)
+                {
+                    replay = false;
+                }
 
+            }
+           while(replay);
+        
 
 
 
